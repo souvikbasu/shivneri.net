1b19f35c4c451a3ed47e46e73cef4fd6ad901d13
Chebrolu <Sailaja.Chebrolu@tesco.com>
2017-10-17 11:51:39 +0530

    add Fort and FortList components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FortList from './FortList.jsx';
+import forts from './data/forts';
 
 class App extends Component {
   render() {
@@ -10,6 +12,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <FortList forts={forts}/>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/data/forts.js
@@ -0,0 +1,31 @@
+const forts = [
+					{
+					fortImage_url:"http://placehold.it/120",
+          fortName:"Golconda",
+          fortPlace:"Hyderabad - Telangana",
+          fortConstructedYear:1500
+          },
+
+          {
+         fortImage_url:"https://placehold.it/120",
+          fortName:"Kamlah Fort",
+          fortPlace:"Kamlah - Himachal Pradesh",
+          fortConstructedYear:1625
+          },
+
+          	{
+					fortImage_url:"http://placehold.it/120",
+          fortName:"Golconda",
+          fortPlace:"Hyderabad - Telangana",
+          fortConstructedYear:1500
+          },
+
+          {
+         fortImage_url:"https://placehold.it/120",
+          fortName:"Kamlah Fort",
+          fortPlace:"Kamlah - Himachal Pradesh",
+          fortConstructedYear:1625
+          }
+];
+
+export default forts;
